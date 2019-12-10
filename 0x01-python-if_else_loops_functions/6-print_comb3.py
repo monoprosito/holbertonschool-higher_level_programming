d845417f20a14d07b88dc3c7c38f5a68d505c02e
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-10 15:36:22 -0500

    Prints only the numbers that the first digit will be greater than the second

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i != j and i < 9:
+        if (i != j and i < j) and i < 9:
             if (i == 8 and j == 9):
                 print('{0}{1}'.format(i, j))
             else:
