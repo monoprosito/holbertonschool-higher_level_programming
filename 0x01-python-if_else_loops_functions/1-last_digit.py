d2e2cc7b7ee8853224fda60a9891347c81ba36e3
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-10 13:52:37 -0500

    Prints the last digit of a random number between -10000 and 10000

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number >= 0:
+    last_digit = number % 10
+elif number < 0:
+    last_digit = (number * -1) % 10
+
+if last_digit > 5:
+    str = 'Last digit of {0} is {1} and is greater than 5'
+elif last_digit < 6 and last_digit != 0:
+    str = 'Last digit of {0} is {1} and is less than 6 and not 0'
+elif last_digit == 0:
+    str = 'Last digit of {0} is {1} and is 0'
+
+print(str.format(number, last_digit))
