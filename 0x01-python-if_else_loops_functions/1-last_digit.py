aacc68c5c9e6ceeaaf33722d9d64e42405754522
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-10 13:57:23 -0500

    Fixes the last digit printed of a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
 elif number < 0:
-    last_digit = (number * -1) % 10
+    last_digit = ((number * -1) % 10) * -1
 
 if last_digit > 5:
     str = 'Last digit of {0} is {1} and is greater than 5'
