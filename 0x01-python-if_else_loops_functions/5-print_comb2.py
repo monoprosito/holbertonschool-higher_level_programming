5e5de6ef975208ec99197a8f6fcc9ecae2ae3733
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-10 15:14:52 -0500

    Refactoring the printing of numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 9:
-        print('0{0}, '.format(i), end='')
-    elif i < 99:
-        print('{0}, '.format(i), end='')
+    if i < 99:
+        print('{:02d}, '.format(i), end='')
     else:
         print(i)
