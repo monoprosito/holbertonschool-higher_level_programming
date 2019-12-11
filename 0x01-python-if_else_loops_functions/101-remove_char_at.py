48dfcd7a9dfba5ec0adae7fa247f497d5951d84a
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-11 00:15:23 -0500

    Documents the function to remove a position from a string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    """Deletes the character at the n position of the str
+    This function creates a copy of the string, deletes
+    a position of the passed string and returns a string
+    without the deleted position
+
+    Parameters
+    ----------
+    str : str
+        The string from which the position needs to be removed
+    n : int
+        The position of the string to be deleted
+
+    Returns
+    -------
+    str
+        The copy of the string without the deleted position
+    """
+
     new_str = ""
 
     for i in range(len(str)):
