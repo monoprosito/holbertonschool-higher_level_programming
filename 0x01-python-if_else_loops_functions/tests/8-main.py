f0a8eef7157a29487f38e7368e4371a2c46490a9
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-11 17:25:38 -0500

    Adds the test for the 8-uppercase script

---
+++ 0x01-python-if_else_loops_functions/tests/8-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+sys.path.append('../')
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("holberton")
+uppercase("Holberton School 98 Battery street")
