8b04df6df8c40717f9c6e35920bbc72c1d4b6fa8
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 14:24:42 -0500

    Remove the alias of the imported module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as add_op
+from add_0 import add
 
 # The code only is executed when isn't imported by __import__
 if __name__ == "__main__":
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     """
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_op.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
