18ffe3b552404c5e457e01c008b4d407a2789939
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 14:27:18 -0500

    Makes the import only if the actual file is executed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
 # The code only is executed when isn't imported by __import__
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
