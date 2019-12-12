7e930201c81738ad16ab959caa41b8a3e9f25514
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 15:27:50 -0500

    Aditionally, checks when the param is only one

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,11 +9,15 @@ if __name__ == '__main__':
 
     """
     av = sys.argv
-    l_av = len(av)
+    l_av = len(av) - 1
 
     if l_av > 1:
-        print(l_av - 1, 'arguments:')
-        for i in range(1, l_av):
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
             print('{:d}: {}'.format(i, av[i]))
-    else:
-        print(l_av - 1, 'arguments.')
+    elif l_av == 1:
+        print(l_av, 'argument:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
