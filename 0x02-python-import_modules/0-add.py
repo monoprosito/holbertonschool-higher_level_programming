c062362f2b074b37514febcd6b14693032132853
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 14:30:48 -0500

    Moves again the import to the top and comments the code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
+
 if __name__ == "__main__":
-    from add_0 import add
+    """
+
+    Prints the result of the addition between two numbers
+
+    """
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
