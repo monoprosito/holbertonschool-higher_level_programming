0fe7a71496c85933e0ba85eb6db0c8bafaacd5fb
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 14:49:53 -0500

    Removes the alias of the imported module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
     """
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     """
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, calc.add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, calc.sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, calc.mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, calc.div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
