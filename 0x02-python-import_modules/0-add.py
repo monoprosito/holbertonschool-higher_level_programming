54c0fbb81a31ec60fae82eda960fd288fb3fe8e5
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-12 14:20:13 -0500

    Replace a simple quotas by double quotas

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     """
     a = 1
     b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add_op.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_op.add(a, b)))
