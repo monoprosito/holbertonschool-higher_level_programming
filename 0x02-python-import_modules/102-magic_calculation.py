fb137f22024bb612260fa81ae7d7222120d0872c
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-12 21:40:50 -0500

    Decodes a Bytecode - #3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from magic_calculation_102 import add, sub
+import dis
+
+def magic_calculation(a, b):
+    if (a < b):
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+    else:
+        return sub(a, b)
+
+dis.dis(magic_calculation)
