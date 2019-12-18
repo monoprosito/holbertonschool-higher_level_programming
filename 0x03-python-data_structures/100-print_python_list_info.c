72d5cafa26f8a30d9df7127d59117d21af729834
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-18 01:35:25 -0500

    Replaces path headers by system headers

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
 #include <stdio.h>
 
 void print_python_list_info(PyObject *p)
