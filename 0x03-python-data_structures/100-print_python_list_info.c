276952b5abdaf82ed398cca28e88d2d280ef53e1
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-18 00:47:08 -0500

    Prints some basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include "new_header.h"
+
+void print_python_list_info(PyObject *p)
+{
+    int i = 0, list_len = 0;
+    PyObject *item;
+    PyListObject *clone = (PyListObject *) p;
+
+    list_len = Py_SIZE(p);
+    printf("[*] Size of the Python List = %d\n", list_len);
+    printf("[*] Allocated: %d\n", (int) clone->allocated);
+
+    for (; i < list_len; ++i)
+    {
+        item = PyList_GET_ITEM(p, i);
+        printf("Element %d: %s\n", i, item->ob_type->tp_name);
+    }
+
+    return;
+}
