7464034656783f1672bd5e54ff2498af842b99ff
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-18 01:52:50 -0500

    Back to the olds headers and make the file executable

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,6 @@
-#include <Python.h>
+#include "/usr/include/python3.4/Python.h"
+#include "/usr/include/python3.4/listobject.h"
+#include "/usr/include/python3.4/object.h"
 #include <stdio.h>
 
 void print_python_list_info(PyObject *p)
