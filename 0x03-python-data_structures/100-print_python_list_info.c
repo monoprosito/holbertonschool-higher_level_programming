e1986ce5934591c4a6537e440469a6124044f5a1
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-18 00:51:04 -0500

    Adds the header of printf function

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,7 @@
 #include "/usr/include/python3.4/Python.h"
 #include "/usr/include/python3.4/listobject.h"
 #include "/usr/include/python3.4/object.h"
+#include <stdio.h>
 
 void print_python_list_info(PyObject *p)
 {
