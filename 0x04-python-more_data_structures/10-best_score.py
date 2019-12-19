ed72a871009af492b52f721a8f30faf78318b75c
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-19 18:30:23 -0500

    Returns the true max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if type(a_dictionary) is dict:
-        return max(a_dictionary, default=None)
+    if a_dictionary is None:
+        return None
+
+    b_score = max(a_dictionary.values(), default=None)
+    for k, v in a_dictionary.items():
+        if v == b_score:
+            return str(k)
