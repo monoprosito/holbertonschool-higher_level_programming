0fa6e3deaf866cf527515f80f6ac138a57c65cb6
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-19 18:02:39 -0500

    Only return the max if the dictionary is of type dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if type(a_dictionary) is dict:
-        if len(a_dictionary) == 0:
-            return None
-
-        return max(a_dictionary)
-
-    return None
+        return max(a_dictionary, default=None)
