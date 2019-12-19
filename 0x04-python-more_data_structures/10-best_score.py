0f3e3cf7201fdde07ff3fa87ae607df09650d2a9
Santiago Arboleda L <monoprosito@gmail.com>
2019-12-19 17:51:48 -0500

    Checks the exact type of the dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if type(a_dictionary) is not dict:
         if len(a_dictionary) == 0:
             return None
 
