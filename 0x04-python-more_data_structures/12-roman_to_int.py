f92df1004c90c79a0610bb4b8fc942512a8fd113
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-20 01:32:17 -0500

    Reorder the conditionals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
-        return 0
+    if type(roman_string) is str and roman_string is not None:
+        if len(roman_string) == 0:
+            roman_letters = [
+                ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+                ['X', 10], ['V', 5], ['I', 1]
+            ]
+            num = 0
+            last = 0
+            rep = 1
 
-    if len(roman_string) == 0:
-        return 0
+            for letter in roman_string:
+                for elem in roman_letters:
+                    if letter == elem[0]:
+                        if last == 0 or last >= elem[1]:
+                            num += elem[1]
+                            rep += last
 
-    roman_letters = [
-        ['M', 1000], ['D', 500],
-        ['C', 100], ['L', 50],
-        ['X', 10], ['V', 5], ['I', 1]
-    ]
-    num = 0
-    last = 0
-    rep = 1
+                            if last != elem[1]:
+                                rep = 1
+                        elif last < elem[1]:
+                            if rep > 1:
+                                num = (num - rep) + (elem[1] - rep)
+                            else:
+                                num = elem[1] - last
 
-    for letter in roman_string:
-        for elem in roman_letters:
-            if letter == elem[0]:
-                if last == 0 or last >= elem[1]:
-                    num += elem[1]
-                    rep += last
+                        last = elem[1]
 
-                    if last != elem[1]:
-                        rep = 1
-                elif last < elem[1]:
-                    if rep > 1:
-                        num = (num - rep) + (elem[1] - rep)
-                    else:
-                        num = elem[1] - last
+            return num
 
-                last = elem[1]
-
-    return num
+    return 0
