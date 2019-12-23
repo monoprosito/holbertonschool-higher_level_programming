e9ae918112d3c3468db562380d52579519842e60
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-23 11:53:11 -0500

    Applies only use of map function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x**2, y)) for y in matrix[:]]
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix[:]))
