f6c3beb645c56fb58c77d8b8081b6b25eb318b51
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-12-24 14:08:08 -0500

    Fixes the path of the Python header file

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,4 @@
-#include "/home/msarboleda/.pyenv/versions/3.4.3/include/python3.4m/Python.h"
+#include "/usr/include/python3.4/Python.h"
 #include <stdio.h>
 
 void print_hexn(const char *str, int n)
