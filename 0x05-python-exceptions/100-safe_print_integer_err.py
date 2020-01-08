c6583ae1eb844e25a84073b003b76f8b3adea18a
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-08 17:41:10 -0500

    Prints an integer with an exception message in stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+        return True
+    except ValueError as ve:
+        sys.stderr.write("Exception: " + str(ve) + "\n")
+        return False
