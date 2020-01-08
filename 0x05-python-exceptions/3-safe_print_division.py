5d8a38bdadd89eefd086cc933532c2c964a3ca2c
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-08 16:02:37 -0500

    Change the print format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print('Inside result:', result)
+        print('Inside result: {}'.format(result))
         return result
