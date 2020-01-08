628754f2bb1787ac90e35207d1fb7a3b0b4239dc
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-08 16:00:59 -0500

    Divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print('Inside result:', result)
+        return result
