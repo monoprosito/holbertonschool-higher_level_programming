8569971697247b13e80686c77d03e5f938c5ba9f
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-09 17:46:17 -0500

    Defines a Square class with a private inst. attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square Class
+
+A Square Class
+
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
