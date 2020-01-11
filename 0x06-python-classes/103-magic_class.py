7a6a7862e8eda3016eb343c3f3d49114e7b2aa91
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-11 03:04:08 -0500

    Only change a default param, import the dis lib and remove parentheses from class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import dis
 import math
 
 
-class MagicClass():
-    def __init__(self, radius):
+class MagicClass:
+    def __init__(self, radius=0):
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
