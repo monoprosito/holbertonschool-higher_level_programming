2508a35ad26139d3750ef5133cba3144b88e9277
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-11 01:17:55 -0500

    Updates again the condition of TypeError

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,7 +70,7 @@ class Square:
     def position(self, position):
         if type(position) is not tuple \
            or type(position[0]) is not int or position[0] < 0 \
-           and type(position[1]) is not int or position[1] < 0:
+           or type(position[1]) is not int or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__position = position
