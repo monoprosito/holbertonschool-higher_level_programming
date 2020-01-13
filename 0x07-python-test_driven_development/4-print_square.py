f134f7114ca918ff914623ce69fc01ad3c01c2a2
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-13 10:18:19 -0500

    Creates a doctest file that test the printing of a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+
+This module prints a square with the character '#' of any positive integer size.
+
+Example:
+
+    ###
+    ###
+    ###
+
+    * The size must be an integer greater than 0.
+
+"""
+
+
+def print_square(size):
+    """
+
+    Prints the names
+
+    Args:
+        size (int): The size of the square to prints.
+
+    Raises:
+        TypeError: If `size` isn't integer.
+        ValueError: If `size` is less than 0.
+
+    """
+
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            print('#', end='')
+
+            if j % size == 0 and j > 0:
+                print()
