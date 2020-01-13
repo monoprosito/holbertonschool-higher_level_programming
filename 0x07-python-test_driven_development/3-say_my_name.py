5e0a06b405a593286b5cb3d41942236c638ba767
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-13 09:20:51 -0500

    Improves the documentation and update the order of tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 """
 
+This module prints the name of a anything
 
+Output: 'My name is <first name> <last name>'
+
+    * The first name and last name must be a strings
 
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
+
+    Prints the names
+
+    Args:
+        first_name (str): The first name of anything.
+        last_name (str): The last name of anything.
+
+    Raises:
+        TypeError: If `first_name` and `last_name` aren't strings.
+
     """
 
     if type(first_name) is not str:
