d89251b9dea43756076cece2e5e34fb89cede865
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-13 23:33:40 -0500

    Fixes the format of the output at prints text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,69 @@
 #!/usr/bin/python3
+"""
+
+This module prints a text with a 2 new lines after each of
+these characters: `.`, `?`, `:`
+
+Example:
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.$
+    $
+    Quonam modo?$
+    $
+    Utrum igitur tibi litteram videor an totas paginas commovere?$
+    $
+    Non autem hoc:$
+    $
+
+    * text must be a string
+
+    * There should be no space at the beginning or
+    at the end of each printed line
+
+"""
+
+
 def text_indentation(text):
+    """
+
+    Prints a text with indentation
+
+    Args:
+        text (str): The text to prints.
+
+    Raises:
+        TypeError: If `text` isn't string.
+
+    """
+
     if type(text) is not str:
         raise TypeError('text must be a string')
 
     text_length = len(text)
     idx = 0
     new_string = ''
+    starting = True
 
     while idx < text_length:
+        if text[idx] == ' ' and starting is True:
+            idx += 1
+            continue
+
+        starting = False
+
         if text[idx] in '.?:':
             new_string += text[idx]
             new_string += '\n'
             new_string += '\n'
             idx += 1
 
-            if idx < text_length:
-                while text[idx] == ' ':
-                    idx += 1
+            while idx < text_length and text[idx] == ' ':
+                idx += 1
 
-        new_string += text[idx]
-        idx += 1
+            continue
+
+        if idx < text_length:
+            new_string += text[idx]
+            idx += 1
 
     print(new_string, end='')
