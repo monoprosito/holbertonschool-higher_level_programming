9077e1b7a592dd52d42e5ce00adbd4a47bfd81de
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-20 20:24:25 -0500

    Computes the area of a Rectangle and overrides str() built-in

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
