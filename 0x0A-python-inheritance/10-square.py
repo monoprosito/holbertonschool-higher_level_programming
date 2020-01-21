80d1ffc45b32586eec7db2a6fb29221e8f50058a
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 01:01:16 -0500

    Removes the area method from this module

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,9 +6,4 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
-        self.integer_validator("size", size)
-
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
