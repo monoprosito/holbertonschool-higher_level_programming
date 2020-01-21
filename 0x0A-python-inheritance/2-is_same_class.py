27b4be16f60a9b124d023906dbd5a072a98adcf8
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-20 23:53:13 -0500

    Adds documentation support for this module

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    """
+    Checks if `obj` is exactly an instance of the specified class
+
+    Args:
+        obj (any): The object to compare
+        a_class (any): The class to compare with the object
+
+    Returns:
+        `True` if the object is exactly an instance of the
+        specified class; otherwise `False`
+    """
+
     if type(obj) == a_class:
         return True
 
