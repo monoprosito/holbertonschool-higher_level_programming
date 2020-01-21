8e83c81bb4ccb64333dafbef196ee3a25d716050
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 18:00:35 -0500

    Checks if the file already exists

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+from os import path
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if len(argv) == 1:
-    save_to_json_file([], 'add_item.json')
-else:
+if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-    for i in range(1, len(argv)):
-        obj_json_file.append(argv[i])
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-    save_to_json_file(obj_json_file, 'add_item.json')
+save_to_json_file(obj_json_file, 'add_item.json')
