c18160d466b9efdf140df4028f8dc7dc77cbd6d7
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 01:05:35 -0500

    Validates the input

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,5 +5,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
