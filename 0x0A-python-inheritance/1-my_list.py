dcbf97c2ae02ae3500fd22428bc2aec90453901d
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-20 22:50:52 -0500

    Adds documentation support to this module

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+A module to prints a list in ascending order
+"""
+
+
 class MyList(list):
+    """
+    A class to customize the list class
+    """
+
     def print_sorted(self):
+        """
+        Prints a list in ascending order
+
+        Sort a list and then prints on the output
+        """
+
         if issubclass(MyList, list):
             print(sorted(self))
