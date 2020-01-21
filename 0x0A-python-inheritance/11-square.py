0b25174672756b04ef5f55f237f69d9da54c3503
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 01:10:15 -0500

    Updates the overriding of __str__ magic method

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from math import sqrt
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     def __init__(self, size):
+        self.integer_validator(size, size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
-        sqroot = int(sqrt(self.area()))
-        return '[Square] ' + str(sqroot) + '/' + str(sqroot)
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
