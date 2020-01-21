bd80d846c39476d85b95411901cc3a5348b571f0
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 17:39:00 -0500

    Removes the printed new line at prints all the file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding='utf-8') as f:
-        tot_lines = len(f.readlines())
+        text = f.readlines()
+        tot_lines = len(text)
 
         if nb_lines <= 0 or nb_lines >= tot_lines:
             f.seek(0)
-            print(f.read())
+            print(f.read(), end='')
         else:
-            f.seek(0)
             for i in range(nb_lines):
-                print(f.readline(), end='')
+                print(text[i], end='')
