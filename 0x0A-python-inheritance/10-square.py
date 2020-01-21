23698af1d0ce381400b868e877fddafa36f42ed6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-20 21:11:17 -0500

    Write a class Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
