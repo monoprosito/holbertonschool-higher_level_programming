689cdd380fc2d04f2a89890267fa39e838cca7f7
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 17:24:43 -0500

    Filter a list of keys to show info about the Student class

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        class_d = self.__dict__
+        sel_d = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
+
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
+
+            return sel_d
+
+        return class_d
