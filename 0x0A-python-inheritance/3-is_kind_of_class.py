8dd3398ee05771b7cb721183e17e73bb054e66d9
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-20 23:58:05 -0500

    Adds documentation support for this module

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
+    """
+    Checks if `obj` is the same class or inherit from `a_class`
+
+    Args:
+        obj (any): The object to compare
+        a_class (any): The class to compare with the object
+
+    Returns:
+        `True` if the object is an instance or inherit from the
+        specified class; otherwise `False`
+    """
+
     return isinstance(obj, a_class)
