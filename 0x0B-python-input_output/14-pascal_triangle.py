59d06745c3303808b78a4b8924ce001fde29d3a8
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-21 20:54:17 -0500

    Returns a representation of the Pascal's triangle of n

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    limit = n - 1
+    triangle = [[1]]
+
+    for i in range(limit):
+        row = []
+        row.append(1)
+
+        if len(triangle[i]) > 1:
+            prev_row_len = len(triangle[i]) - 1
+            nxt = 1
+
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
+                nxt += 1
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
