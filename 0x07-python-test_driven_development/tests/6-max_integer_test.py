a9d8cedb3da52b14d90b2b2b6de84a9a323e045c
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-23 12:24:24 -0500

    Adds unit test for a Max Integer function

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+import math
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    A class to test a max integer function
+    """
+
+    def test_max_integer(self):
+        """
+        Test the max integer in a list of integers when the integers
+        are positive or negative numbers
+        """
+        self.assertIsNone(max_integer([]))
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
+        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
+        self.assertAlmostEqual(max_integer([1.0, 1.5, 1.6, 3.7, 2.3]), 3.7)
+
+    def test_wrong_types(self):
+        """
+        Test the max integer with wrong parameters types
+        """
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+        with self.assertRaises(TypeError):
+            max_integer(["Monty", 89, 34, -9.7, "Python"])
+
+        with self.assertRaises(TypeError):
+            max_integer(4)
+
+        with self.assertRaises(TypeError):
+            max_integer([3, 5.7, 7])
+
+        with self.assertRaises(TypeError):
+            max_integer([1e400, 1e500, 1e600])
+
+        with self.assertRaises(TypeError):
+            max_integer(float('nan'))
+
+        with self.assertRaises(TypeError):
+            max_integer('Monty Python')
+
+        with self.assertRaises(TypeError):
+            max_integer((1, 0))
