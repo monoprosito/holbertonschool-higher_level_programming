c9f6a4ad836ad8f9fef71c21c1bf4fc58c4923c4
Santiago Arboleda L <monoprosito@gmail.com>
2020-01-27 12:46:40 -0500

    Adds comments to the Rectangle methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,45 +29,72 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        ...
+        """
         return self.__width
 
     @width.setter
     def width(self, param):
+        """
+        ...
+        """
         self.check_integer_parameter(param, 'width')
 
         self.__width = param
 
     @property
     def height(self):
+        """
+        ...
+        """
         return self.__height
 
     @height.setter
     def height(self, param):
+        """
+        ...
+        """
         self.check_integer_parameter(param, 'height')
 
         self.__height = param
 
     @property
     def x(self):
+        """
+        ...
+        """
         return self.__x
 
     @x.setter
     def x(self, param):
+        """
+        ...
+        """
         self.check_integer_parameter(param, 'x')
 
         self.__x = param
 
     @property
     def y(self):
+        """
+        ...
+        """
         return self.__y
 
     @y.setter
     def y(self, param):
+        """
+        ...
+        """
         self.check_integer_parameter(param, 'y')
 
         self.__y = param
 
     def check_integer_parameter(self, value, param):
+        """
+        ...
+        """
         if type(value) is not int:
             raise TypeError(param + ' must be an integer')
 
@@ -78,9 +105,15 @@ class Rectangle(Base):
             raise ValueError(param + ' must be >= 0')
 
     def area(self):
+        """
+        ...
+        """
         return self.__width * self.__height
 
     def display(self):
+        """
+        ...
+        """
         if self.__y > 0:
             print('\n' * self.__y, end='')
 
@@ -91,11 +124,17 @@ class Rectangle(Base):
             print('#' * self.__width)
 
     def __str__(self):
+        """
+        ...
+        """
         return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
             self.id, self.x, self.y, self.width, self.height
         )
 
     def update(self, *args, **kwargs):
+        """
+        ...
+        """
         argc = len(args)
         kwargc = len(kwargs)
         modif_attrs = ['id', 'width', 'height', 'x', 'y']
@@ -112,6 +151,9 @@ class Rectangle(Base):
                     setattr(self, k, v)
 
     def to_dictionary(self):
+        """
+        ...
+        """
         return {
             'id': self.id,
             'width': self.width,
