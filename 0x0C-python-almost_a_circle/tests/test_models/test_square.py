d164d5e0e640f027060ef62e7a36ea7405b4e876
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-27 22:14:01 -0500

    Fixes the PEP8 route to checks on the Square test

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -19,7 +19,7 @@ class TestSquare(unittest.TestCase):
         Test that checks PEP8
         """
         syntax = pep8.StyleGuide(quit=True)
-        check = syntax.check_files(['models/rectangle.py'])
+        check = syntax.check_files(['models/square.py'])
         self.assertEqual(
             check.total_errors, 0,
             "Found code style errors (and warnings)."
