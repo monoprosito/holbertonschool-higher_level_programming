3d6bfeb034afbea5361a9620fefe369c1c841dbb
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-02 15:58:23 -0500

    Counts the records with an specific id in a table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Displays the number of records with `id` = '89'
+-- in the table `first_table` of the database
+-- `hbtn_0c_0` in MySQL Server.
+SELECT COUNT(id) FROM first_table WHERE `id` = '89';
