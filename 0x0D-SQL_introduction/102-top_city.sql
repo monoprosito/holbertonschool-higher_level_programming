9b393efb6774787b2dda51d03c71bdd4507fcbf6
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-02 18:56:34 -0500

    Displays the top 3 of cities temperatures during July and August

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Displays the top 3 of cities temperature during
+-- July and August ordered by temperature (descending).
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = '7' OR month = '8'
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
