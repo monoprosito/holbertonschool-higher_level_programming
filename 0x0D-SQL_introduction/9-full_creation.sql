498132edfe83bde451d46b5234c66338b349cf09
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-02 16:04:35 -0500

    Creates a new table and adds multiples rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates a table `second_table` in the
+-- database `hbtn_0c_0` in MySQL Server and
+-- adds multiples rows.
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
