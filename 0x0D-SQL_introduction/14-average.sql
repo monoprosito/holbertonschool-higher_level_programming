95a34cd9822f8307fac1a27cc4b25ddff1b6897b
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-02 17:35:00 -0500

    Computes the average of a column in a table

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Computes the `score` average of all records in
+-- the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+
+SELECT AVG(`score`) as `average`
+FROM second_table;
