fd324c13863e93786447e7e455067f9bc644bff0
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-03 15:01:14 -0500

    Creates a MySQL Server user and sets a temporary password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates a MySQL Server user `user_0d_1`
+-- and sets a temporary password `user_0d_1_pwd`.
+CREATE USER IF NOT EXISTS `user_0d_1`;
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
