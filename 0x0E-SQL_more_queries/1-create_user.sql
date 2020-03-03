5e5dcbf1d5e93b6e02542f71b13dcfef082877ce
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-03 15:05:12 -0500

    Grant privileges and flush privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 -- and sets a temporary password `user_0d_1_pwd`.
 CREATE USER IF NOT EXISTS `user_0d_1`;
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
