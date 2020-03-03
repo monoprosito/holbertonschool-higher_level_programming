15f9f14e69b237b8279d7d83eac5c4181e5d2a30
Santiago Arboleda L <monoprosito@gmail.com>
2020-03-03 15:20:28 -0500

    Updates the name of the created user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Creates a MySQL Server user `user_0d_1`
 -- and sets a temporary password `user_0d_1_pwd`.
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER `user_0d_1` IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd'
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
