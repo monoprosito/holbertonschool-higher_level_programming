2a26701f3c7385e045e37524a54b3bda355063f3
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-03 21:23:21 -0500

    Lists all Comedy shows in the database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows
+-- in the database `hbtn_0d_tvshows`.
+
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE name = 'Comedy'
+ORDER BY tv_shows.title ASC;
