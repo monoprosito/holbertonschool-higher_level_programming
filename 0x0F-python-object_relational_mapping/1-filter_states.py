d801cc7b1d738c7855109446681caec720448b31
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-29 01:59:34 -0500

    Order the data by the state id in ascending order

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE 'N%' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
