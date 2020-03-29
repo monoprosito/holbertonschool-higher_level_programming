64e087fbf0afc8e065a9faa84a35832d8f2f509e
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-29 16:47:17 -0500

    Updates the error message

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     instance = session.query(State).filter(State.name == argv[4]).first()
 
     if instance is None:
-        print('Nothing')
+        print('Not found')
     else:
         print('{0}'.format(instance.id))
