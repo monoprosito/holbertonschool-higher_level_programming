672e08241177490a2a201c45f9db641e8f977bbb
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-29 13:57:51 -0500

    The LIKE clausule finds for case insensitive results

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name = '{}' \
+                 WHERE name LIKE BINARY '{}' \
                  ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
 
