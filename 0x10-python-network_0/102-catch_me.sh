0fef9fd54b59ea3dac851bead894251376518a59
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-07 17:41:48 -0500

    Makes a request that causes a special respond
    This script makes a request to 0.0.0:5000/catch_me
    that causes the server to respond with a message containing
    "You got me!", in the body of the response.

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script makes a request to '0.0.0.0:5000/catch_me' that causes the server to respond with a message containing 'You got me!', in the body of the response.
+curl -L 0.0.0.0:5000/catch_me -X PUT -H "Origin: HolbertonSchool" -d "user_id=98"
