d0c1ef2868e10115b16914a507471443d24ce34f
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-14 14:07:52 -0500

    Fetches https://intranet.hbtn.io/status with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
