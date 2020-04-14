ea8b34a99eec52687a6632d64d0d70d27f1bce3e
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-14 17:05:35 -0500

    Sends a request and manage the HTTP status code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+with `requests` module.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+
+    if req.status_code == requests.codes.ok:
+        print(req.text)
+    else:
+        print('Error code:', req.status_code)
