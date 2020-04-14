005e1a9c383636871eb3d0f686e9dcab5f3ab547
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-14 15:40:42 -0500

    Displays the value of the variable X-Request-Id in the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL
+and displays the value of the `X-Request-Id`
+variable found in the header of the response.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print(req.headers.get('X-Request-Id'))
