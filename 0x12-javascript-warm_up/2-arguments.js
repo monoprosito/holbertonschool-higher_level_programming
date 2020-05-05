a876a6507979b0bd494c0a641b9a34a9c9ed9ad6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-05 23:27:55 +0000

    Fixes semistandard syntax

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length > 3)
-    console.log('Arguments found');
-else if (process.argv.length == 3)
-    console.log('Argument found');
-else
-    console.log('No argument');
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
