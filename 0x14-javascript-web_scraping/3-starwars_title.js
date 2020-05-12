5783499b7f74e4cfad89c33aa36dc6ba1f5b0239
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 17:47:27 +0000

    Uppercase the not found message

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,11 @@
 const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(starWarsUri, function (_err, _res, body) {
+request(starWarsUri, function (_err, res, body) {
   body = JSON.parse(body);
 
   if (body.detail) {
-    console.log(body.detail);
+    console.log(res.statusCode + ' ' + body.detail.toUpperCase());
   } else {
     console.log(body.title);
   }
