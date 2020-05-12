903062e05a7ddee4e81f0f71916b10c29137449c
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 18:31:42 +0000

    Fixes the JSON parse

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,8 @@ const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starWarsUri, function (err, _res, body) {
+  body = JSON.parse(body);
+
   if (err == null) {
     console.log(err);
   } else {
