ea2406c0724b3569224054c241722100427160f7
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 17:20:10 +0000

    Reads a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
