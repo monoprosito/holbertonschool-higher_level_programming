788792ce5c66d54082100b86bf9c5dd6444c1d51
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 18:30:41 +0000

    Checks for null

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
-const param = process.argv[2];
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-if (parseInt(param)) {
-  request(starWarsUri.concat(param), function (_err, _res, body) {
-    body = JSON.parse(body);
+request(starWarsUri, function (err, _res, body) {
+  if (err == null) {
+    console.log(err);
+  } else {
     console.log(body.title);
-  });
-} else {
-  console.log('404 NOT FOUND');
-}
+  }
+});
