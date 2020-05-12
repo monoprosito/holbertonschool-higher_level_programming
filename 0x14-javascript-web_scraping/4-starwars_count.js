8d342adf489cc35d882986463942f49125d6d981
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 20:04:50 +0000

    Prints the number of movies where a specific character is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+let times = 0;
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
+
+  for (let i = 0; i < body.length; ++i) {
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
+      }
+    }
+  }
+
+  console.log(times);
+});
