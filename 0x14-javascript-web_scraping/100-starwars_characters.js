f465f0aa1982d9a1cc766d4ad898a866591cb7ca
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 23:33:14 +0000

    Prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
+  }
+});
