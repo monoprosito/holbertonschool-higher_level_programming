83e4346aafb479bae493a38ba401563b2c7377a4
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 19:16:28 +0000

    Reverses changes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,7 @@
 const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-if (process.argv[2] === '7') {
-  console.log('The Force Awakens');
-} else {
-  request(starWarsUri, function (_err, _res, body) {
-    body = JSON.parse(body);
-    console.log(body.title);
-  });
-}
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
