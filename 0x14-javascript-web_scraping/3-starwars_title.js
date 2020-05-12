2b9d83c5a6bb046cc077335ba41cd6ce445b1f88
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 17:42:56 +0000

    Prints not found url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,11 @@ const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starWarsUri, function (_err, _res, body) {
-  console.log(JSON.parse(body).title);
+  body = JSON.parse(body);
+
+  if (body.detail) {
+    console.log(body.detail);
+  } else {
+    console.log(body.title);
+  }
 });
