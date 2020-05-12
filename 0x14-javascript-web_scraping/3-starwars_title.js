c1b862400c9d4a018a112ca10c3f685de71fff30
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 18:36:30 +0000

    Removes a conditional

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,7 @@
 const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(starWarsUri, function (err, _res, body) {
+request(starWarsUri, function (_err, _res, body) {
   body = JSON.parse(body);
-
-  if (body.title) {
-    console.log(body.title);
-  } else {
-    console.log(err);
-  }
+  console.log(body.title);
 });
