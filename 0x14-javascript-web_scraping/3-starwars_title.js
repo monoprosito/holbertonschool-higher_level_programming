0ab0a924c1f06068952cf7ddfad04f0dca0b0aa9
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 17:51:48 +0000

    Checks the ID argument passed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+const param = process.argv[2];
 
-request(starWarsUri, function (_err, res, body) {
-  body = JSON.parse(body);
-
-  if (body.detail) {
-    console.log(res.statusCode + ' ' + body.detail.toUpperCase());
-  } else {
+if (parseInt(param)) {
+  request(starWarsUri.concat(param), function (_err, _res, body) {
+    body = JSON.parse(body);
     console.log(body.title);
-  }
-});
+  });
+}
