935c173a6ee2693389bd0dd931a4745e737293ea
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-12 01:01:02 +0000

    Checks for Rectangle class parameters

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Checks for Rectangle parameters
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width !== 'number' || typeof height !== 'number' || width <= 0 || height <= 0) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = width;
+      this.height = height;
+    }
+  }
+};
